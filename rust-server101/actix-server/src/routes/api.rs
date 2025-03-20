0b8d7a3ddb7c671661225f20a1f0f41fc03d64f5
Jakob Vestergaard Olason <jakobolason@gmail.com>
2025-03-20 17:13:45 +0100

    added the rust server

---
+++ rust-server101/actix-server/src/routes/api.rs
@@ -0,0 +1,40 @@
+use actix_web::{web, HttpResponse};
+use serde::{Deserialize, Serialize};
+use mongodb::Client;
+
+pub fn api_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api")
+            .route("/save", web::post().to(save_data))
+            .route("/status", web::get().to(get_status))
+    );
+}
+
+const DB_NAME: &str = "test";
+const COLL_NAME: &str = "users";
+
+async fn get_status() -> HttpResponse {
+    HttpResponse::Ok().body("API Status: OK")
+}
+
+async fn save_data(
+    form: web::Json<User>,
+    client: web::Data<Client>
+                    ) -> HttpResponse 
+{
+    let collection = client.database(DB_NAME).collection(COLL_NAME);
+    let result = collection.insert_one(form.into_inner()).await;
+    match result {
+        Ok(_) => HttpResponse::Ok().body("user added"),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+// query or body format (struct)
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+pub struct User {
+    pub first_name: String,
+    pub last_name: String,
+    pub username: String,
+    pub email: String,
+}
