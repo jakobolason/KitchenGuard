f8ec51ff335633031a132b3e1ce0d33cdeb429d0
WilliamNB2003 <williamnobo2003@gmail.com>
2025-05-13 14:40:24 +0200

    changed HelathCheckSTruct

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -61,10 +61,11 @@ pub struct HealthData {
     pub kitchen_pir: String,
     pub living_room_pir: String,
     pub bathroom_pir: String,
-    pub LED: String,
-    pub PowerPlug: String,
-    pub Bridge: String,
-    pub PI: String,
+    pub bathromm_LED: String,
+    pub living_room_LED: String,
+    pub powerPlug: String,
+    pub bridge: String,
+    pub pi: String,
 }
 
 async fn health_check(form: web::Json<HealthData>) -> HttpResponse {
